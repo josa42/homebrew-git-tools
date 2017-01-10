0f92e2c999e53e63e1ac7e3121311d37cf04cc1f
Josa Gesell <josa@gesell.me>
2017-01-10 11:42:32 +0100

    Update git-release to 0.1.0

--- Formula/git-release.rb
+++ Formula/git-release.rb
@@ -1,9 +1,11 @@
 class GitRelease < Formula
-  desc ""
+  desc "Quickly tag and push new versions"
+  version "0.1.0"
+
   homepage "https://github.com/josa42/git-release"
   head "https://github.com/josa42/git-release.git"
-  # url "https://github.com/josa42/git-release/archive/1.0.0.tar.gz"
-  # sha256 ""
+  url "https://github.com/josa42/git-release/archive/#{version}.tar.gz"
+  sha256 "30b00a54eba85fa76fb83e4b7f2da208cb62d4e3db3e69932c9686b88eb3d42e"
 
   depends_on "go" => :build
   depends_on "glide" => :build
