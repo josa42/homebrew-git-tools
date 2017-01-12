11a76ff4bcad4b2e1146b3f9bda553184e2c28da
Josa Gesell <josa@gesell.me>
2017-01-12 13:48:19 +0100

    ⬆️  Update git-cleanup to 0.1.0

--- Formula/git-cleanup.rb
+++ Formula/git-cleanup.rb
@@ -1,11 +1,11 @@
 class GitCleanup < Formula
   desc ""
-  # version "0.1.0"
+  version "0.1.0"
 
   homepage "https://github.com/josa42/git-cleanup"
   head "https://github.com/josa42/git-cleanup.git"
-  # url "https://github.com/josa42/git-cleanup/archive/#{version}.tar.gz"
-  # sha256 ""
+  url "https://github.com/josa42/git-cleanup/archive/#{version}.tar.gz"
+  sha256 "f7e3556e08acb449a3f69166a5206b0769135b85e36d53cab258783e32e6f387"
 
   depends_on "go" => :build
   depends_on "glide" => :build
