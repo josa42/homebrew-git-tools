7a3210b649e3478081cca6c1a7a7c91d0079eb6c
Josa Gesell <josa@gesell.me>
2017-06-26 11:26:36 +0200

    ⬆️  Update git-repo to 0.5.0

--- Formula/git-repo.rb
+++ Formula/git-repo.rb
@@ -1,23 +1,20 @@
 class GitRepo < Formula
   desc "Quickly open repo urls in the browser"
-  version "0.4.1"
+  version "0.5.0"
 
   homepage "https://github.com/josa42/git-repo"
   head "https://github.com/josa42/git-repo.git"
   url "https://github.com/josa42/git-repo/archive/#{version}.tar.gz"
-  sha256 "eec55b2836d7431ff0ef97414154335847eed46607f79713396beb90390c3fd0"
+  sha256 "2ffbff7acc85384a1c97776c0fd4a0a60fee27f7cc15179170b7ab759d910e59"
 
   depends_on "go" => :build
-  depends_on "glide" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
 
     dir = buildpath/"src/github.com/josa42/git-repo"
     dir.install Dir["*"]
     cd dir do
-      system "glide", "install"
       system "go", "build", "-o", bin/"git-repo"
     end
   end
