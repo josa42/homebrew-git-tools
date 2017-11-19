c5551cb66367d68418e5586c954e71bff5459132
Josa Gesell <josa@gesell.me>
2017-11-19 11:16:16 +0100

    ⬆️  Update git-repo to 0.5.5

--- Formula/git-repo.rb
+++ Formula/git-repo.rb
@@ -1,11 +1,11 @@
 class GitRepo < Formula
   desc "Quickly open repo urls in the browser"
-  version "0.5.4"
+  version "0.5.5"
 
   homepage "https://github.com/josa42/git-repo"
   head "https://github.com/josa42/git-repo.git"
   url "https://github.com/josa42/git-repo/archive/#{version}.tar.gz"
-  sha256 "c39b89f4def12701329cbfd1f90536cbd38251937dcf3d4a7d87d4aad2467cad"
+  sha256 "b087906fcbc1e9aaa71a9554833b42788904dac0a7e7e7dd215b8a1d9736c868"
 
   depends_on "go" => :build
 
