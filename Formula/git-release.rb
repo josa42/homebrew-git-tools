5c4970a75334de7b22eda25196fe1d6ddbf6703a
Josa Gesell <josa@gesell.me>
2018-05-15 11:54:25 +0200

    ⬆️  Update git-release to 0.6.2

--- Formula/git-release.rb
+++ Formula/git-release.rb
@@ -1,11 +1,11 @@
 class GitRelease < Formula
   desc "Quickly tag and push new versions"
-  version "0.1.0"
+  version "0.6.2"
 
   homepage "https://github.com/josa42/git-release"
   head "https://github.com/josa42/git-release.git"
   url "https://github.com/josa42/git-release/archive/#{version}.tar.gz"
-  sha256 "30b00a54eba85fa76fb83e4b7f2da208cb62d4e3db3e69932c9686b88eb3d42e"
+  sha256 "13f7e0438069a616f3099334d3d68e10ec03a5d603ebb1bdc2078728839b9339"
 
   depends_on "go" => :build
   depends_on "glide" => :build
