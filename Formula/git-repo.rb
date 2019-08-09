0a333402c172f79ce01aa1d3ce673c0e7a615983
Josa Gesell <josa@gesell.me>
2019-08-09 16:11:04 +0200

    ⬆️  Update git-repo to 0.6.0

--- Formula/git-repo.rb
+++ Formula/git-repo.rb
@@ -1,22 +1,16 @@
 class GitRepo < Formula
   desc "Quickly open repo urls in the browser"
-  version "0.5.7"
+  version "0.6.0"
 
   homepage "https://github.com/josa42/git-repo"
   head "https://github.com/josa42/git-repo.git"
   url "https://github.com/josa42/git-repo/archive/#{version}.tar.gz"
-  sha256 "e68aead79cc24d1c66e12672f086ca5e8548fe129441ca052f0856e58cafe305"
+  sha256 "90d18e2d5517ee0bfac156662d3d3f2f6be0b29f4760279c49c41983cf0a1e75"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-
-    dir = buildpath/"src/github.com/josa42/git-repo"
-    dir.install Dir["*"]
-    cd dir do
-      system "go", "build", "-o", bin/"git-repo"
-    end
+    system "go", "build", "-o", bin/"git-repo"
   end
 
   test do
