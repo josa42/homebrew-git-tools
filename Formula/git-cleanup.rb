12047c5728d4640f96aeb7ca0006b2f35d05c83e
Josa Gesell <josa@gesell.me>
2019-08-09 15:59:36 +0200

    ⬆️  Update git-cleanup to 0.3.0

--- Formula/git-cleanup.rb
+++ Formula/git-cleanup.rb
@@ -1,25 +1,16 @@
 class GitCleanup < Formula
   desc ""
-  version "0.2.0"
+  version "0.3.0"
 
   homepage "https://github.com/josa42/git-cleanup"
   head "https://github.com/josa42/git-cleanup.git"
   url "https://github.com/josa42/git-cleanup/archive/#{version}.tar.gz"
-  sha256 "414cbcdd21bfc4256a795b6464276291f89f4409143d357e9bfd3231e23fe77f"
+  sha256 "8685b0655b87b483e3699f05c43ad58b2f3fcab7e2e987dadcf85f97775468bf"
 
   depends_on "go" => :build
-  depends_on "glide" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
-
-    dir = buildpath/"src/github.com/josa42/git-cleanup"
-    dir.install Dir["*"]
-    cd dir do
-      system "glide", "install"
-      system "go", "build", "-o", bin/"git-cleanup"
-    end
+    system "go", "build", "-o", bin/"git-cleanup"
   end
 
   test do
