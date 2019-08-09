dcecfbaaefabafab833f89545a4ce82ce1679917
Josa Gesell <josa@gesell.me>
2019-08-09 15:54:43 +0200

    ⬆️  Update git-bus-factor to 0.3.0

--- Formula/git-bus-factor.rb
+++ Formula/git-bus-factor.rb
@@ -1,22 +1,16 @@
 class GitBusFactor < Formula
   desc ""
-  version "0.2.0"
+  version "0.3.0"
 
   homepage "https://github.com/josa42/git-bus-factor"
   head "https://github.com/josa42/git-bus-factor.git"
   url "https://github.com/josa42/git-bus-factor/archive/#{version}.tar.gz"
-  sha256 "bb59dae120fa35f9387d9c88fe0227ce3da4f649ad879dd6d4c4ea49021b05ab"
+  sha256 "272b7c2c7990564bf3bb4d4951834d9558773af1f4d9f73c1191ba49dd9f5f50"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    
-    dir = buildpath/"src/github.com/josa42/git-bus-factor"
-    dir.install Dir["*"]
-    cd dir do
-      system "go", "build", "-o", bin/"git-bus-factor"
-    end
+    system "go", "build", "-o", bin/"git-bus-factor"
   end
 
   test do
