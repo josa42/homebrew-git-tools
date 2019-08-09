62ff829e2733293f697cb8560983560e96950b8b
Josa Gesell <josa@gesell.me>
2019-08-09 16:08:12 +0200

    ⬆️  Update git-release to 0.8.0

--- Formula/git-release.rb
+++ Formula/git-release.rb
@@ -1,25 +1,16 @@
 class GitRelease < Formula
   desc "Quickly tag and push new versions"
-  version "0.7.0"
+  version "0.8.0"
 
   homepage "https://github.com/josa42/git-release"
   head "https://github.com/josa42/git-release.git"
   url "https://github.com/josa42/git-release/archive/#{version}.tar.gz"
-  sha256 "b2091bf3fd6938dde3d5b3a03a4cef1d994cda63517f65bb1c4e88d5b3d6e898"
+  sha256 "834466fa8a7c0fd455f58ce247c847610711a976525cccf1b4ff554d108bb51c"
 
   depends_on "go" => :build
-  depends_on "glide" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
-
-    dir = buildpath/"src/github.com/josa42/git-release"
-    dir.install Dir["*"]
-    cd dir do
-      system "glide", "install"
-      system "go", "build", "-o", bin/"git-release"
-    end
+    system "go", "build", "-o", bin/"git-release"
   end
 
   test do
