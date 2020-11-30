aad2fd09c36d27d489f474eb300295adf3a5f8d9
Josa Gesell <josa@gesell.me>
2020-12-01 00:25:17 +0100

    ⬆️  Update git-release to 0.10.1

--- Formula/git-release.rb
+++ Formula/git-release.rb
@@ -1,11 +1,11 @@
 class GitRelease < Formula
   desc "Quickly tag and push new versions"
-  version "0.10.0"
+  version "0.10.1"
 
   homepage "https://github.com/josa42/git-release"
   head "https://github.com/josa42/git-release.git"
   url "https://github.com/josa42/git-release/archive/v#{version}.tar.gz"
-  sha256 "d854405e5f0eee6a6c55d42663dbf988827815877e3e948c0c4b2bd3c91805a3"
+  sha256 "d1511a5c93bc93e101461ef1f2c70a40ff48cfff17bc2a6be2e40f4a4f328c1c"
 
   depends_on "go" => :build
 
