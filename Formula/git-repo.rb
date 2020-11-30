bc20e4a82641ad153215b6e954ee4d7ad9c45dd4
Josa Gesell <josa@gesell.me>
2020-11-30 22:08:03 +0100

    ⬆️  Update git-repo to 0.7.1

--- Formula/git-repo.rb
+++ Formula/git-repo.rb
@@ -1,11 +1,11 @@
 class GitRepo < Formula
   desc "Quickly open repo urls in the browser"
-  version "0.7.0"
+  version "0.7.1"
 
   homepage "https://github.com/josa42/git-repo"
   head "https://github.com/josa42/git-repo.git"
-  url "https://github.com/josa42/git-repo/archive/#{version}.tar.gz"
-  sha256 "14882d20bf3b93abf7b670af2ece44a10e5865264387d1b12b3f3842c0861122"
+  url "https://github.com/josa42/git-repo/archive/v#{version}.tar.gz"
+  sha256 "2e8f0abdfad13ab09e016f1b1180439dad538ee20b86ba948444dccf735ba0fd"
 
   depends_on "go" => :build
 
--- bin/update.sh
+++ bin/update.sh
@@ -8,16 +8,18 @@ version=$2
 
 url=$(cat $filePath | grep 'url "' | sed 's/^.*url "//' | sed 's/"$//' | sed "s/#{version}/$version/")
 
-
 tmpFile=/tmp/git-tools-update-$name-$version
-wget $url -O $tmpFile
+curl -sSL $url > $tmpFile
 hash=$(sha256sum $tmpFile | sed 's/ .*//')
 rm $tmpFile
 
-cat $filePath | sed "s/version \".*/version \"$version\"/" | sed "s/sha256.*/sha256 \"$hash\"/" > $filePath.tmp
+v="$(echo $version | sed 's/^v//')"
+cat $filePath \
+  | sed "s/version \".*/version \"$v\"/" \
+  | sed "s/sha256.*/sha256 \"$hash\"/" \
+  > $filePath.tmp
 
 cat $filePath.tmp
-
 
 read -p "Are you sure? " -n 1 -r
 echo
@@ -29,6 +31,8 @@ fi
 
 rm $filePath
 mv $filePath.tmp $filePath
+
+brew test $filePath || exit 1
 
 git add $filePath
 git commit -m "⬆️  Update $name to $version"
