26b1a6ba57a6654442f9328a00de9cf7734d9083
Josa Gesell <josa@gesell.me>
2020-12-01 01:07:38 +0100

    ⬆️  Update git-cleanup to 0.3.1

--- Formula/git-cleanup.rb
+++ Formula/git-cleanup.rb
@@ -1,11 +1,11 @@
 class GitCleanup < Formula
   desc ""
-  version "0.3.0"
+  version "0.3.1"
 
   homepage "https://github.com/josa42/git-cleanup"
   head "https://github.com/josa42/git-cleanup.git"
-  url "https://github.com/josa42/git-cleanup/archive/#{version}.tar.gz"
-  sha256 "8685b0655b87b483e3699f05c43ad58b2f3fcab7e2e987dadcf85f97775468bf"
+  url "https://github.com/josa42/git-cleanup/archive/v#{version}.tar.gz"
+  sha256 "bce29bc5ce3581a825d1e9db85909b465f785782a93f3b832700fe397f104fa8"
 
   depends_on "go" => :build
 
